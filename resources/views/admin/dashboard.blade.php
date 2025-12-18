e9fdb09cfca9b77021e9cdcf8efa074e82cfab03
mayakawai <maya24ti@mahasiswa.pcr.ac.id>
2025-12-19 01:47:17 +0700

    .

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -77,7 +77,7 @@
       <div class="col-12 grid-margin stretch-card">
         <div class="card">
           <div class="card-body">
-            <h4 class="card-title">Peserta Terbaru</h4>
+            <h4 class="card-title">Daftar Peserta Seminar</h4>
             <div class="table-responsive">
               <table class="table table-hover table-bordered">
                 <thead>
